b22b7457dc75a65d6daf4cbcda919bacaa9538da
NATA <118177362+LifeLong-LeArneR17@users.noreply.github.com>
2023-09-03 17:07:00 +0300

    Update images.js

--- src/components/services/images.js
+++ src/components/services/images.js
@@ -7,5 +7,5 @@ export const photoDataFetch = async (query, page) => {
     const {data} = await axios.get(`https://pixabay.com/api/?q=${query}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`)
     return data.hits
 }
-
+// efef
 
